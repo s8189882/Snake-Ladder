a171c2431d615f2c9872139713115d41e647731f
s8189882 <s8189882@gmail.com>
2022-08-16 14:24:17 +0530

    Displaying Welcome message

---
+++ Snake & Ladder Simulator/src/javapractice/SnakenLadder.java
@@ -0,0 +1,11 @@
+package javapractice;
+
+public class SnakenLadder {
+	
+	public static void main(String args[]) {
+
+		System.out.println("Welcome to Snake & Ladder Simulator!");
+		System.out.println("This problem simulates a Snake and Ladder Game.");
+		System.out.println("The Player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved.");
+	}
+}
