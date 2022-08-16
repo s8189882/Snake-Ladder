56e5e768e3102da43f22bffd007e28773bc3c0b7
s8189882 <109401577+s8189882@users.noreply.github.com>
2022-08-16 16:36:00 +0530

    Update SnakenLadder.java

--- Snake & Ladder Simulator/src/javapractice/SnakenLadder.java
+++ Snake & Ladder Simulator/src/javapractice/SnakenLadder.java
@@ -4,53 +4,53 @@ public class SnakenLadder {
 		
 	public static void main(String args[]) {
 
-    int rollDice = 0;
-    int dice = 0;
-    int playOption = 0;
-    int POSITION = 0;
+	    int rollDice = 0;
+	    int dice = 0;
+	    int playOption = 0;
+	    int POSITION = 0;
 
 
-    System.out.println("Welcome to Snake & Ladder Simulator!");
-    System.out.println("This problem simulates a Snake and Ladder Game.");
-    System.out.println("The Player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved.");
+	    System.out.println("Welcome to Snake & Ladder Simulator!");
+	    System.out.println("This problem simulates a Snake and Ladder Game.");
+	    System.out.println("The Player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved.");
 
-    System.out.println("Let's begin the game...");
-    System.out.println("Initial position is Square Zero");
-    System.out.println("Position : Square " + POSITION);
+	    System.out.println("Let's begin the game...");
+	    System.out.println("Initial position is Square Zero");
+	    System.out.println("Position : Square " + POSITION);
 
 
-    for (dice=1; POSITION < 100; dice++) {
+	    for (dice=1; POSITION < 100; dice++) {
 
-      System.out.println();
-      rollDice = (int)((Math.random()*6)+1);
-      System.out.println("Rolled Dice Value : " + rollDice);
+	      	System.out.println();
+	   	rollDice = (int)((Math.random()*6)+1);
+	      	System.out.println("Rolled Dice Value : " + rollDice);
 
-      playOption = (int) Math.floor(Math.random() * 10) % 3;
+	      	playOption = (int) Math.floor(Math.random() * 10) % 3;
 
-      switch (playOption) {
-        case 0 :
-          System.out.println("You encountered a LADDER!");
-          System.out.println("You can move " + rollDice +" places ahead.");
-          POSITION += rollDice;
-          break;
+	      switch (playOption) {
+		case 0 :
+		  System.out.println("You encountered a LADDER!");
+		  System.out.println("You can move " + rollDice +" places ahead.");
+		  POSITION += rollDice;
+		  break;
 
-        case 1 :
-          System.out.println("You encountered a SNAKE!");
-          System.out.println("You will move " + rollDice +" places back.");
-          POSITION -= rollDice;
-          break;
+		case 1 :
+		  System.out.println("You encountered a SNAKE!");
+		  System.out.println("You will move " + rollDice +" places back.");
+		  POSITION -= rollDice;
+		  break;
 
-        case 2 :
-          System.out.println("You got NO PLAY");
-          System.out.println("You will have to skip your turn.");
+		case 2 :
+		  System.out.println("You got NO PLAY");
+		  System.out.println("You will have to skip your turn.");
 
-       }
+	       }
 
-      if (POSITION < 0)
-      POSITION = 0;
+	      if (POSITION < 0)
+	      POSITION = 0;
 
-      System.out.println("Current Position : Square " + POSITION);
-		
-		 }
+	      System.out.println("Current Position : Square " + POSITION);
+
+	}
 	}
 }
