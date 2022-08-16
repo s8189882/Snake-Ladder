e5c15d4f97663f41e839fe6193791b3b69c88da1
s8189882 <109401577+s8189882@users.noreply.github.com>
2022-08-16 16:22:33 +0530

    Update SnakenLadder.java

--- Snake & Ladder Simulator/src/javapractice/SnakenLadder.java
+++ Snake & Ladder Simulator/src/javapractice/SnakenLadder.java
@@ -7,17 +7,17 @@ public class SnakenLadder {
 	
 	public static void main(String args[]) {
 		
-    System.out.println("Welcome to Snake & Ladder Simulator!");
-    System.out.println("This problem simulates a Snake and Ladder Game.");
-    System.out.println("The Player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved.");
+	    System.out.println("Welcome to Snake & Ladder Simulator!");
+	    System.out.println("This problem simulates a Snake and Ladder Game.");
+	    System.out.println("The Player starts from 0 rolls the die to get a number between 1 to 6, finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved.");
 
-    System.out.println("Let's begin the game...");
-    System.out.println("Initial position is Square Zero");
-    System.out.println("Position : Square " + POSITION);
+	    System.out.println("Let's begin the game...");
+	    System.out.println("Initial position is Square Zero");
+	    System.out.println("Position : Square " + POSITION);
 
-    int rollDice = 0;
+	    int rollDice = 0;
 
-    rollDice = (int)((Math.random()*6)+1);
-    System.out.println("Rolled Dice Value :" + rollDice);
+	    rollDice = (int)((Math.random()*6)+1);
+	    System.out.println("Rolled Dice Value :" + rollDice);
 	}
 }
