275fce0b3dbb6e5d650fbee1da7073f2fa4e8eed
s8189882 <s8189882@gmail.com>
2022-08-16 15:16:24 +0530

    Player rolls the die to get a number between 1 to 6

--- Snake & Ladder Simulator/src/javapractice/SnakenLadder.java
+++ Snake & Ladder Simulator/src/javapractice/SnakenLadder.java
@@ -3,11 +3,13 @@ package javapractice;
 public class SnakenLadder {
 	
 	public static final int POSITION = 0;
+	public static final int ROLL_DICE = 0;
 	
 	public static void main(String args[]) {
 
-		System.out.println("Let's begin the game...");
-		System.out.println("Initial position is Square Zero");
-		System.out.println("Position : Square " + POSITION);
+		int rollDice = 0;
+		
+		rollDice = (int)((Math.random()*6)+1);
+	    System.out.println("Rolled Dice Value :" + rollDice);
 	}
 }
